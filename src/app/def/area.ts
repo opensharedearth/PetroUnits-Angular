f4e016ee94e59278a7455182d876dc0de27d73ea
Thomas <thomas.rundle@conduitlearning.com>
2017-01-30 11:35:02 -0600

    Added several area unit definitions

--- src/app/def/area.ts
+++ src/app/def/area.ts
@@ -18,5 +18,30 @@ export const AREA_UNITS = [
       name: 'ft²',
       baseUnit: 'square meter',
       baseValue: 0.092903
+    },
+    {
+      name: 'km²',
+      baseUnit: 'square meter',
+      baseValue: 1000000
+    },
+    {
+      name: 'yd²',
+      baseUnit: 'square meter',
+      baseValue: 0.836127
+    },
+    {
+      name: 'in²',
+      baseUnit: 'square meter',
+      baseValue: 0.00064516
+    },
+    {
+      name: 'mi²',
+      baseUnit: 'square meter',
+      baseValue: 2589990
+    },
+    {
+      name: 'hectare',
+      baseUnit: 'square meter',
+      baseValue:  10000
     }
 ];
